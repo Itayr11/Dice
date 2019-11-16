b1bc4f7aeb3efb72e07320451006a0592c448abe
Itayr11 <56202111+Itayr11@users.noreply.github.com>
2019-11-16 13:03:57 +0200

    update2 names

--- index.js
+++ index.js
@@ -3,47 +3,49 @@ var count2 = 1;
 var player1name;
 var player2name;
 
-function Dice()
-{
+function Dice() {
+  if (document.getElementsByClassName("player1name")[0].innerHTML != "Player 1")
+  {
+    var randomNumber1 = Math.random();
+  randomNumber1 = Math.floor(randomNumber1 * 6 + 1);
 
-var randomNumber1 = Math.random();
-randomNumber1 = Math.floor(randomNumber1*6+1);
+  var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
 
- var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
-
-var image1 = document.getElementsByClassName("img1")[0];
+  var image1 = document.getElementsByClassName("img1")[0];
 
 
-image1.setAttribute("src", randomImageSource1);
+  image1.setAttribute("src", randomImageSource1);
 
-var randomNumber2 = Math.random();
-randomNumber2 = Math.floor(randomNumber2*6+1);
+  var randomNumber2 = Math.random();
+  randomNumber2 = Math.floor(randomNumber2 * 6 + 1);
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-var image2 = document.getElementsByClassName("img2")[0];
+  var image2 = document.getElementsByClassName("img2")[0];
 
-image2.setAttribute("src", randomImageSource2);
+  image2.setAttribute("src", randomImageSource2);
 
-if(randomNumber1>randomNumber2)
-{
-document.querySelector('h1').innerHTML = player1name+ " Wins";
-document.getElementsByClassName("player1count")[0].innerHTML = count1;
-count1++;
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = player1name + " Wins";
+    document.getElementsByClassName("player1count")[0].innerHTML = count1;
+    count1++;
+  } else if (randomNumber1 == randomNumber2)
+    document.querySelector('h1').innerHTML = "Tie";
+  else {
+    document.querySelector('h1').innerHTML = player2name + " Wins";
+    document.getElementsByClassName("player2count")[0].innerHTML = count2;
+    count2++;
+
+  }
 }
-else if (randomNumber1==randomNumber2)
-document.querySelector('h1').innerHTML = "Tie";
-else
-{
-document.querySelector('h1').innerHTML = player2name + " Wins";
-document.getElementsByClassName("player2count")[0].innerHTML = count2;
-count2++;
-}
+else {
+    alert("Update Names First!!");
+  }
 }
 
-function updatenames(){
- player1name = document.getElementById("player1name").value;
- player2name = document.getElementById("player2name").value;
-document.getElementsByClassName("player1name")[0].innerHTML = player1name;
-document.getElementsByClassName("player2name")[0].innerHTML = player2name;
+function updatenames() {
+  player1name = document.getElementById("player1name").value;
+  player2name = document.getElementById("player2name").value;
+  document.getElementsByClassName("player1name")[0].innerHTML = player1name;
+  document.getElementsByClassName("player2name")[0].innerHTML = player2name;
 }
